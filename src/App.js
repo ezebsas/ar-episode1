7dbb9814af33da529dc30275a3eb2919633b88bd
Unknown <emollinedo@untref.edu.ar>
2020-12-23 17:14:57 -0300

    Banner styled

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
-import React from "react";
+import React, {useEffect} from "react";
 import "./styles/App.scss";
 import Header from './components/header'
 import Banner from './components/banner'
 
 function App() {
+  useEffect(() => {
+    let vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty('--vh', `${vh}px`); //here we define the 1 percent of the inner height and set it to the --vh variable
+  }, [])
   return (
     <div className='App'>
       <Header />
