3431cbc51eb58eb670bd783e251502549fd7c974
Unknown <emollinedo@untref.edu.ar>
2020-12-29 02:22:12 -0300

    Creating navigation component and adding react-router-dom
    Until adding navigation at the bottom, unstyled

--- src/App.js
+++ src/App.js
@@ -1,8 +1,28 @@
 import React, {useEffect} from 'react'
+import { Route } from 'react-router-dom'
 import gsap from 'gsap';
 import "./styles/App.scss";
+
+//Components
 import Header from './components/header'
+import Navigation from './components/navigation'
+
+//pages
 import Home from './pages/home';
+import CaseStudies from './pages/caseStudies';
+import Approach from './pages/approach';
+import Services from './pages/services';
+import About from './pages/about';
+
+const routes = [
+  {path: '/', name: 'Home', Component: Home}, 
+  {path: '/case-studies', name: 'Case Studies', Component: CaseStudies}, 
+  {path: '/approach', name: 'Approach', Component: Approach}, 
+  {path: '/services', name: 'Services', Component: Services}, 
+  {path: '/about-us', name: 'About Us', Component: About}
+]
+
+//routes
 
 function App() {
   useEffect(() => {
@@ -14,7 +34,14 @@ function App() {
   return (
     <>
       <Header />
-      <Home />
+      <div className="App">
+        {routes.map(({path, Component}) => (
+          <Route key={path} exact path={path}>
+            <Component/>
+          </Route>
+        ))}
+      </div>
+      <Navigation />
     </>
   );
 }
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,59 +1,70 @@
-import React, {useEffect} from 'react'
+import React, { useEffect, useState } from 'react'
 import gsap from 'gsap';
 import Banner from '../components/banner'
 import Cases from '../components/cases'
 import IntroOverlay from "../components/introOverlay";
 
+const tl = gsap.timeline();
+
+const homeAnimation = (completeAnimation) => {
+  tl.from('.line span', {
+    duration: 1.8, 
+    y: 100,
+    ease: 'power4.out',
+    delay: 1,
+    skewY: 7,
+    stagger: {
+      amount: 0.3
+    }
+  }).to('.overlay-top', {
+    duration: 1.6,
+    height: 0,
+    ease: 'expo.inOut',
+    stagger: 0.4
+  }).to('.overlay-bottom', {
+    duration: 1.6,
+    width: 0,
+    ease: 'expo.inOut',
+    delay: -0.8,
+    stagger: {
+      amount: 0.4
+    }
+  }).to('.intro-overlay', {
+    duration: 0,
+    css: {
+      display: 'none'
+    }
+  }).from('.case-image img', {
+    duration: 1.6,
+    scale: 1.4,
+    ease: 'expo.inOut',
+    delay: -2,
+    stagger: {
+      amount: 0.4
+    },
+    onComplete: completeAnimation // we wanna run this function when the animation is finsished
+  });
+}
+
 const Home = () => {
+  const [animationComplete, setAnimationComplete] = useState(false);
+
+  const completeAnimation = () => {
+    setAnimationComplete(true)
+  };
+
   useEffect(() => {
     //timeline
-    const tl = gsap.timeline();
-
-    tl.from('.line span', {
-      duration: 1.8, 
-      y: 100,
-      ease: 'power4.out',
-      delay: 1,
-      skewY: 7,
-      stagger: {
-        amount: 0.3
-      }
-    }).to('.overlay-top', {
-      duration: 1.6,
-      height: 0,
-      ease: 'expo.inOut',
-      stagger: 0.4
-    }).to('.overlay-bottom', {
-      duration: 1.6,
-      width: 0,
-      ease: 'expo.inOut',
-      delay: -0.8,
-      stagger: {
-        amount: 0.4
-      }
-    }).to('.intro-overlay', {
-      duration: 0,
-      css: {
-        display: 'none'
-      }
-    }).from('.case-image img', {
-      duration: 1.6,
-      scale: 1.4,
-      ease: 'expo.inOut',
-      delay: -2,
-      stagger: {
-        amount: 0.4
-      }
-    });
+    homeAnimation(completeAnimation);
 
   }, [])
   return (
-    <div>
+    <>
+    {animationComplete === false ? <IntroOverlay /> : ''}
       <IntroOverlay />
       <Banner />
       <Cases />
-      
-    </div>
+    </>
   )
 }
 
