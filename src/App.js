0be54e3f1cc5f18a3e0810550bcd474e397a39c1
Unknown <emollinedo@untref.edu.ar>
2020-12-29 10:39:45 -0300

    handle resizing continually

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useEffect} from 'react'
+import React, { useEffect, useState } from 'react'
 import { Route } from 'react-router-dom'
 import gsap from 'gsap';
 import "./styles/App.scss";
@@ -25,14 +25,33 @@ const routes = [
 //routes
 
 function App() {
+  const [dimensions, setDimensions] = useState({
+    height: window.innerHeight,
+    width: window.innerWidth,
+  })
   useEffect(() => {
     let vh = window.innerHeight * 0.01;
     document.documentElement.style.setProperty('--vh', `${vh}px`); //here we define the 1 percent of the inner height and set it to the --vh variable
 
     gsap.to('body', {duration: 0, css: {visibility: 'visible'}}); //this sholud be for preventing white text from flashing, but its not working ...
+
+    const HandleResize = () => { //this is a bit redundant because we already defined this but we want to update every single time the window resizes
+      setDimensions({
+        height: window.innerHeight,
+        width: window.innerWidth,
+      })
+    }
+
+    window.addEventListener('resize', HandleResize)
+
+    return () => {
+      window.removeEventListener('resize', HandleResize)
+    }
+
   }, [])
   return (
     <>
+      {console.log(dimensions)}
       <Header />
       <div className="App">
         {routes.map(({path, Component}) => (
